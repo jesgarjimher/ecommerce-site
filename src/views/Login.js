8c35818f553c2e99cdbb2dac4ef549fa54e1fa19
Jesus Garcia <jesgarjimher@gmail.com>
2025-12-27 13:10:42 +0100

    ft: added simple login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,20 +1,45 @@
 import Headers from "./Header"
 import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function Login() {
+    const [email,setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const navigate = useNavigate();
+
     useEffect(() => {
         if(localStorage.getItem("user-info")) {
             navigate("/addproduct");
         }
-    })
+    },[]);
+
+
+    async function login() {
+        console.log("email " + email, "password " + password)
+        let item = {email, password};
+        let result = await fetch("http://localhost:8000/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            },
+            body:JSON.stringify(item)
+        });
+        result = await result.json();
+        localStorage.setItem("user-info",JSON.stringify(result))
+        navigate("/addproduct");
+    }
     return(
         <>
             <Headers/>
-            <div>
+            <div className="col-sm-6 offset-sm-3">
                 <h1>Login page</h1>
+                <input type="text" onChange={(e) => setEmail(e.target.value)} placeholder="email" className="form-control"></input>
+            <br/>
+            <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="password" className="form-control"></input>
+            <br/>
+            <button onClick={login} className="btn btn-primary">Login</button>
             </div>
         </>
         
