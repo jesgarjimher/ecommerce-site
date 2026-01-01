2d63e35eade50c3e176fe812910e94752663d117
Jesus Garcia <jesgarjimher@gmail.com>
2026-01-01 19:59:43 +0100

    ft:added search component

--- src/views/SearchProduct.js
+++ src/views/SearchProduct.js
@@ -1,23 +1,68 @@
+import { Link } from "react-router-dom";
 import Header from "./Header";
 import {useState} from "react";
+import { Table } from "react-bootstrap";
 
 function SearchProduct() {
 
-    const [name,setName] = useState("");
-    const [price,setPrice] = useState("");
-    const [file,setFile] = useState("");
-    const [description,setDescription] = useState("");
+    const [data,setData] = useState([]);
 
-    async function addProduct() {
-        
+    async function search(key) {
+        let result = await fetch("http://localhost:8000/api/search/" + key);
+        result = await result.json();
+        console.log(key)
+        setData(result);
     }
+
+     async function deleteOperation(id) {
+        let result = await fetch("http://localhost:8000/api/delete/" + id, {
+            method: "DELETE"
+        });
+        result = await result.json();
+        search();
+    }
+    
+    
     return(
         <>
             <Header/>
             <div className="col-sm-6 offset-sm-3">
                <h1>Search product</h1>
                <br></br>
-               <input type="text" className="form-control"></input>
+               <input type="text" className="form-control" onChange={(e) => search(e.target.value)}></input>
+            </div>
+
+             <div className="col-sm-8 offset-sm-2">
+                <Table>
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Name</td>
+                            <td>Price</td>
+                            <td>Description</td>
+                            <td>Image</td>
+                            <td>Operations</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                        data.map((item) => 
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td>{item.price}</td>
+                                <td>{item.description}</td>
+                                <td><img className="img-product" src={"http://localhost:8000/storage/" + item.file_path} alt={`Photography of ${item.description}`}></img>{}</td>
+                                <td className="options-td">
+                                    <button className="btn btn-danger" onClick={() => deleteOperation(item.id)}>Delete</button>
+                                    <Link className="btn btn-secondary"  to={"/updateproduct/" + item.id}>Edit</Link>
+                                    </td>
+                            </tr>
+                        )
+                        }
+                    </tbody>
+                
+                </Table>
             </div>
         </>
         
